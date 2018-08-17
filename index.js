d0339187188028d591145118547e250486f23f0e
Yomaira Escano <yjescano@gmail.com>
2018-08-17 04:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name)
+  })
+}
+
+const logDriversByHometown = function (drivers, location) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  })
+}
+
+// const driversByRevenue = function (drivers) {
+//   return drivers.slice().sort(function (driver1, driver2) {
+//     return driver1.revenue - driver2.revenue
+//   })
+// }
+
+function driversByRevenue (drivers) {
+  return drivers.slice().sort(function (firstDriver, secondDriver) {
+    return firstDriver.revenue - secondDriver.revenue
+  })
+}
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (firstDriver, secondDriver) {
+    return firstDriver.name.localeCompare(secondDriver.name)
+  })
+}
+
+// function driversByName (drivers) {
+//   return drivers.sort(function (firstDriver, secondDriver) {
+//     return firstDriver.localeCompare(secondDriver)
+//   })
+// }
+// function logDriverNames (drivers) {
+//   return drivers.map(function (driver) {
+//     console.log(driver.name);
+//   })
+// }
+//
+// function logDriversByHometown (drivers, location) {
+//   return drivers.filter(function (driver) {
+//     if (driver.hometown === location) {
+//       console.log(driver.name)
+//     }
+//   })
+// }
+//
+// function driversByRevenue (drivers) {
+//   const revenueSorter = function (revenue1, revenue2) {
+//       return revenue2 - revenue1
+//   }
+//   return drivers.sort(revenueSorter)
+// }
